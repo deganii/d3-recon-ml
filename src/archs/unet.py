3a6626f48ab58fb5a26e451fe407f47dc949a473
hrd06001 <hillaryd@mit.edu>
2017-11-03 15:58:31 -0400

    Update unet to new dataset format

--- src/archs/unet.py
+++ src/archs/unet.py
@@ -5,11 +5,13 @@ from keras.optimizers import Adam
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 
+K.set_image_data_format('channels_last')
 data_folder = '../../data/'
 data_training = np.load(data_folder + 'ds-lymphoma-training.npz')
 data_testing=np.load(data_folder + 'ds-lymphoma-test.npz')
 train_data, train_labels = data_training['data'], data_training['labels']
-img_rows=img_cols=int(np.sqrt(train_data.shape[1]))
+img_rows, img_cols = train_data.shape[1], train_data.shape[2]
+
 
 def get_unet():
     inputs = Input((img_rows, img_cols, 1))
@@ -48,31 +50,12 @@ def get_unet():
     conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
     conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
 
-
-    #conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
-
     model = Model(inputs=[inputs], outputs=[conv9])
 
 
-    #model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
     model.compile(optimizer=Adam(lr=1e-5), loss='mean_squared_error')
 
     return model
-
-#def preprocess(imgs):
- #   imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
-  #  for i in range(imgs.shape[0]):
-   #     imgs_p[i] = np.reshape(imgs[i], (img_cols, img_rows), preserve_range=True)
-
-#    imgs_p = imgs_p[..., np.newaxis]
- #   return imgs_p
-
-def preprocess(imgs):
-    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=float)
-    for i in range(imgs.shape[0]):
-        imgs_p[i] = np.reshape(imgs[i], (img_rows, img_cols))
-    imgs_p = imgs_p[..., np.newaxis]
-    return imgs_p
 
 model = get_unet()
 model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
@@ -84,5 +67,6 @@ print('-'*30)
 #testing resize function.  Cant get to work for individual line items
 label_real=train_labels[:,0,:]
 
-model.fit(preprocess(train_data), preprocess(label_real), batch_size=32, nb_epoch=10, verbose=1, shuffle=True,
+
+model.fit(train_data, label_real, batch_size=32, nb_epoch=10, verbose=1, shuffle=True,
           validation_split=0.2, callbacks=[model_checkpoint])
