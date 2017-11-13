aee9bd01cb5c8eb8e3f715b63a1c6fd401526b3e
deganii <idegani@mit.edu>
2017-11-13 14:56:32 -0500

    add DSSIM loss function, add histogram plot and SSIM statistics on prediction logic

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,3 +1,4 @@
+import os
 from keras import backend as K
 import numpy as np
 from src.processing.folders import Folders
@@ -10,12 +11,17 @@ class DataLoader(object):
     def load(cls, dataset='ds-lymphoma', set='training', records = -1):
         raw = np.load(Folders.data_folder() + '{0}-{1}.npz'.format(dataset, set), mmap_mode='r')
         if records > 0:
-            return raw['data'][0:records, ...], \
-                raw['labels'][0:records, 0, ...], \
-                raw['labels'][0:records, 1, ...]
+            # additional logic for efficient caching of small subsets of records
+            raw_trunc = Folders.data_folder() + '{0}-{1}-n{2}.npz'.format(dataset, set, records)
+            if os.path.isfile(raw_trunc):
+                raw_n = np.load(raw_trunc, mmap_mode='r')
+                return raw_n['data'], raw_n['labels'][:, 0, ...], raw_n['labels'][:, 1, ...]
+            else :
+                data, labels =  raw['data'][0:records, ...], raw['labels'][0:records, ...]
+                np.savez(raw_trunc, data=data, labels=labels)
+                return data, labels[:, 0, ...], labels[:, 1, ...]
         else:
-            return raw['data'], raw['labels'][:, 0, ...], \
-                   raw['labels'][:, 1, ...]
+            return raw['data'], raw['labels'][:, 0, ...], raw['labels'][:, 1, ...]
 
     @classmethod
     def load_training(self, dataset='ds-lymphoma', records=-1):
---
+++ src/visualization/ssim_plotter.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib
+import matplotlib.mlab as mlab
+from scipy.stats import norm
+
+from src.processing.folders import Folders
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+class SSIMPlotter(object):
+    @classmethod
+    def save_plot(cls, model_name, ssim):
+        # plot and save to disk
+        full_path = Folders.predictions_folder() + model_name + '_ssim.png'
+
+        fig = plt.figure(figsize=(4,2))
+        ax = fig.add_subplot(111)
+        ax.set_xlabel('SSIM')
+        ax.set_ylabel('Count(Prediction)')
+        n, bins, patches = plt.hist(ssim, 50, normed=1, facecolor='green', alpha=0.75)
+
+        # add a 'best fit' gaussian
+        (mu, sigma) = norm.fit(ssim)
+        y = mlab.normpdf(bins, mu, sigma)
+        l = plt.plot(bins, y, 'r--', linewidth=2)
+
+        fig.suptitle('SSIM of ' + model_name + ': $\mu={0:.2f},\ \sigma={1:.2f}$'.format(mu,sigma),
+                     fontsize=10, fontweight='bold')
+        fig.subplots_adjust(left = 0.17)
+        fig.subplots_adjust(bottom = 0.27)
+        fig.subplots_adjust(right = 0.96)
+        fig.subplots_adjust(top = 0.84)
+
+        fig.canvas.draw()
+        plt.grid(True)
+        plt.savefig(full_path)
+        plt.close(fig)
+
+# Test case
+SSIMPlotter.save_plot('unet_test', np.asarray([0.5,0.3,0.4, 0.5,0.5, 0.4, 0.8, 0.2, 0.3, 0.4, 0.4]))
+
