8694d0fa1e531486cb0484029b206dadc11a531e
deganii <idegani@mit.edu>
2019-09-12 01:29:10 -0400

    no need for separate activation

--- src/arch/holo_transfer.py
+++ src/arch/holo_transfer.py
@@ -14,11 +14,9 @@ def get_holo_transfer(img_rows, img_cols, filter_size=32, conv_depth=1,
              optimizer=Adam(lr=1e-4), loss='mean_squared_error', metrics=None,
              output_depth=1, activation: object='relu'):
 
-    advanced_activation = activation
     inputs = Input((img_rows, img_cols, 1))
     conv = Conv2D(conv_depth, (filter_size, filter_size),
                   activation=activation, padding='same')(inputs)
-    conv = advanced_activation()(conv)
 
     model = Model(inputs=[inputs], outputs=[conv])
     if metrics is None:
